ddd27be457fa5f67581d992c936b1fc270b192bb
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2024-01-02 15:16:27 -0600

    feat: streaming OpenAI response

---
+++ main.py
@@ -0,0 +1,16 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+chat = ChatOpenAI(streaming=True)
+
+prompt = ChatPromptTemplate.from_messages([
+  ("human", "{content}")
+])
+
+messages = prompt.format_messages(content="Dime un poema de 4 lineas")
+
+for message in chat.stream(messages):
+  print(message.content)
