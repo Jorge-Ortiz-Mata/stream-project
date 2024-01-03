32211cce448aa69bd3f336f1d79b3bd620b2038c
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2024-01-03 10:12:22 -0600

    feat: finish project

--- main.py
+++ main.py
@@ -1,25 +1,49 @@
+from typing import Any, Optional
+from uuid import UUID
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.chains import LLMChain
+from langchain.callbacks.base import BaseCallbackHandler
 from dotenv import load_dotenv
-
+from queue import Queue
+from threading import Thread
 load_dotenv()
 
-chat = ChatOpenAI(streaming=True)
+queue = Queue()
+
+class StreamingHandler(BaseCallbackHandler):
+  def on_llm_new_token(self, token, **kwargs):
+    queue.put(token)
+
+  def on_llm_end(self, token, **kwargs):
+    queue.put(None)
+
+  def on_llm_error(self, error, **kwargs):
+    queue.put(None)
+
+chat = ChatOpenAI(
+  streaming=True,  
+  callbacks=[StreamingHandler()]
+)
 
 prompt = ChatPromptTemplate.from_messages([
   ("human", "{content}")
 ])
 
-chain = LLMChain(llm=chat, prompt=prompt)
+class StreamingChain(LLMChain):
+  def stream(self, input):
+    def task():
+      self(input)
 
-chain("Dime una broma")
+    Thread(target=task).start()
+    
+    while True:
+      token = queue.get()
+      if token is None:
+        break
+      yield token
 
-print(chain)
+chain = StreamingChain(llm=chat, prompt=prompt)
 
-
-
-# messages = prompt.format_messages(content="Dime un poema de 4 lineas")
-
-# for message in chat.stream(messages):
-#   print(message.content)
+for output in chain.stream(input={"content": "Dime un chiste"}):
+  print(output)
